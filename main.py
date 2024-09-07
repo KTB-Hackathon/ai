a3ed5d8f1cff306906e6cfc0da16a97abfb7706a
DS0708 <jonum12312@gmail.com>
2024-09-07 10:27:27 +0900

    last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import List
@@ -11,6 +12,15 @@ from Recommend import main
 
 app = FastAPI()
 
+# CORS 처리를 위한 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인에서의 접근을 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용
+    allow_headers=["*"]   # 모든 헤더 허용
+)
+
 class Reco(BaseModel):
     TRAVEL_STYL_1: int
     TRAVEL_STYL_5: int
@@ -22,7 +32,6 @@ MONGO_DETAILS = "mongodb://10.178.0.3"
 client = AsyncIOMotorClient(MONGO_DETAILS)
 db = client["imagedb"]  # 데이터베이스 객체 생성
 collection = db["image"]
-
 
 @app.post("/recommend/")
 async def recommend(reco: Reco):
@@ -42,17 +51,15 @@ async def recommend(reco: Reco):
         'TRAVEL_MISSION_INT': 3
     }
     
-    # MongoDB에서 89개의 데이터 중 랜덤으로 15개를 가져오는 쿼리
     cursor = collection.aggregate([{"$sample": {"size": 15}}])
     random_data = await cursor.to_list(length=15)
     
-    # 각 문서에서 필요한 컬럼들을 리스트로 변환
     detailed_results = [
         [
             "https://storage.cloud.google.com/journey_mate_image/"+data.get("uri", ""),
             "" if pd.isna(data.get("road_nm", "")) else data.get("road_nm", ""),
-            data.get("x", 0),  # x는 항상 존재한다고 가정
-            data.get("y", 0),  # y는 항상 존재한다고 가정
+            data.get("x", 0),
+            data.get("y", 0),
             data.get("area", ""),
             data.get("description", "")
         ]
@@ -69,7 +76,6 @@ def process_message(message: Message):
         
         ai_response = get_ai_message(tour_description)
         
-        # 결과를 동일한 구조로 반환
         return {"content": ai_response}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
